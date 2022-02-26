c00bb93a66e17fff335f3f15d150ad045646fd2d
Nithesh Narayanan <nithuan141@gmail.com>
2022-02-26 20:20:55 +0530

    feat: a basic animation on toast

--- src/components/Notification/NotificationList/NotificationList.tsx
+++ src/components/Notification/NotificationList/NotificationList.tsx
@@ -8,7 +8,7 @@ export const NotificationList: FC = () => {
     const { notifications } = useNotification()
 
     return <div className={styles.notification}>
-        {notifications?.reverse().map(notification => <NotificationElement key={notification.id} notification={notification} />)}
+        {notifications?.map(notification => <NotificationElement key={notification.id} notification={notification} />)}
     </div>
 }
 
